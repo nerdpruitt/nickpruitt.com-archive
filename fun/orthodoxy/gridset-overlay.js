9b9ff2b2ea29b3d696ffcc01855f97b577f4f194
Nick Pruitt <npruitt@fa2118427.lifeway.org>
2013-02-19 10:28:30 -0600

    Clean up clean up
    Revived my Orthodoxy section, cleaned up some js. Had fun.

---
+++ fun/orthodoxy/gridset-overlay.js
@@ -0,0 +1,102 @@
+// Gridset Toggle JS
+
+gs = {
+
+	init: function (p, c, w) {
+	
+		gs.bind(document, 'keydown', function (e) { 
+		
+			if (!e) var e = window.event;
+		
+			if(e.metaKey || e.ctrlKey) {
+				
+				switch (e.which || e.keyCode) {
+					case 71:
+					
+						var gw = document.getElementById('gridsetoverlaywrap');
+					
+						if (!gw) gs.show(['da','db','t','m'], [5,5,5,2], [1000,990,768,320]);
+						else document.body.removeChild(gw);
+						
+						gs.prevent(e);
+						break;
+				}
+				
+			}
+		
+		
+		});
+	
+	},
+
+	show: function (p, c, w) {
+	
+		var b = document.getElementsByTagName('body')[0],
+			gw = '<div id="gridwrap"><div id="gridoverlay" class="wrapper">',
+		
+			k = 0, breaks = '',
+			
+			styles = '<style id="gridsetoverlaystyles" type="text/css">#gridsetoverlaywrap{position:static;}#gridwrap {display:block;position:fixed;top:0;left:0;width:100%;height:100%;z-index:1000;pointer-events:none;font-family:Helvetica, Arial, sans-serif !important;}#gridoverlay{position:relative;height:100%;overflow:hidden !important;background:none !important;}#gridoverlay div{display:block;position:static;height:100%;color:#bcbcff;}#gridoverlay .gridset{position:absolute;width:100%;height:100%;top:0;left:0;opacity:0.7;}#gridoverlay .gridset div{text-align:left;font-size:10px !important;border-right:1px solid #bcbcff;border-left:1px solid #bcbcff;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;box-sizing:border-box;}#gridoverlay div small{width:100%;display:block;text-align:center;color:#7D80DB;font-weight:700 !important;border-bottom:1px solid #bcbcff;border-top:1px solid #bcbcff;padding-top:0 !important;background-color:rgb(240,240,255) !important;text-transform:none !important;height:22px !important;line-height:22px !important;}#gridoverlay .gridset:nth-child(2) div{border-style:dashed;padding-top:23px;}#gridoverlay .gridset:nth-child(2) small{border-style:dashed;}#gridoverlay .gridset:nth-child(3) div{border-style:dotted;padding-top:45px;}#gridoverlay .gridset:nth-child(3) small{border-style:dotted;}.noshow{display:none;}</style>';
+						
+		while (p[k]) {
+		
+			var hides = '', 
+				l = 0;
+		
+			if (w[k] != breaks && k == 0) gw += '<div>';
+			else if (w[k] != breaks) gw += '</div><div>';
+		
+			while (p[l]) {
+		
+				if (l != k && w[l] != w[k]) hides += p[l] + '-hide ';
+				l++;			
+		
+			}
+		
+			gw += '<div class="gridset ' + hides + '"><div class="'+p[k]+'1"><small>'+p[k]+'1</small></div>';
+		
+			var i = 1;
+		
+			while (i++ < c[k]) gw += '<div class="'+p[k]+i+'"><small>'+p[k]+i+'</small></div>';
+		
+			gw += '</div>';
+		
+			if (k == w.length - 1) gw += '</div>';
+		
+			breaks = w[k];
+		
+			k++;
+		
+		}
+		
+		gw += '</div></div>';
+		
+		var newgw = document.createElement('div');
+		
+		newgw.id = 'gridsetoverlaywrap';
+		
+		newgw.innerHTML = gw + styles;
+		
+		b.appendChild(newgw);
+	
+	},
+	
+	bind : function (t, e, f) {
+		
+		if (t.attachEvent) t.attachEvent('on' + e, f);
+		else t.addEventListener(e, f, false);
+	
+	},
+	
+	prevent : function (e) {
+	
+		if (e.preventDefault) e.preventDefault();
+		else event.returnValue = false;
+	
+	}
+
+
+};
+
+if (window.location.href.match('gridset=show')) gs.show(['da','db','t','m'], [5,5,5,2], [1000,990,768,320]);
+else gs.init(['da','db','t','m'], [5,5,5,2], [1000,990,768,320]);
